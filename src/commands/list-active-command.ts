ce99602af3af795d038ebb628d7ca9307cb1f546
hopperelec <hopperelec@gmail.com>
2025-05-15 02:13:28 +0100

    A few changes/fixes to `/list-active-trains`
    Reference the current time when finding "timetabled trains"
    List extra trains in the corresponding API categories
    Group late and early trains together under "delayed trains"

--- src/commands/list-active-command.ts
+++ src/commands/list-active-command.ts
@@ -1,7 +1,7 @@
 import {CommandInteraction, EmbedBuilder} from "discord.js";
 import {proxy} from "../bot";
 import {FullTrainsResponse, parseLastSeen, parseTimesAPILocation} from "metro-api-client";
-import {calculateDifferenceToTimetable, timeDateToStr, timeNumbersToStr} from "../timetable";
+import {calculateDifferenceToTimetable, getExpectedTrainState, timeDateToStr, timeNumbersToStr} from "../timetable";
 import {getStationCode, getTodaysTimetable} from "../cache";
 
 function listTrains(trains: string[]) {
@@ -12,29 +12,34 @@ export default async function command(interaction: CommandInteraction) {
     // TODO: This only needs to know in which APIs it is active, might need to add an option in the proxy for presence in props
     const activeTrains = await proxy.getTrains() as FullTrainsResponse;
 
+    const time = timeDateToStr(new Date());
+
     const todaysTimetable = await getTodaysTimetable();
-    const timetabledTrains = Object.keys(todaysTimetable);
+    const timetabledTrains = Object.entries(todaysTimetable)
+        .filter(([_, trainTimetable]) => getExpectedTrainState(trainTimetable, time).state === "active")
+        .map(([trn]) => trn);
     const activeTrainsFromTimesAPI: string[] = [];
     const activeTrainsFromTrainStatusesAPI: string[] = [];
-    const lateTrains: {
-        trn: string,
-        possibleDelays: number[],
-    }[] = [];
-    const earlyTrains: {
+    const delayedTrains: {
         trn: string,
         possibleDelays: number[],
     }[] = [];
     const missingTrains = timetabledTrains.filter(trn => !activeTrains.trains[trn]);
     const extraTrains: string[] = [];
     for (const [trn, data] of Object.entries(activeTrains.trains)) {
-        if (!(trn in todaysTimetable)) {
+        if (data.status.timesAPI) {
+            activeTrainsFromTimesAPI.push(trn);
+        }
+        if (data.status.trainStatusesAPI) {
+            activeTrainsFromTrainStatusesAPI.push(trn);
+        }
+        if (!(trn in timetabledTrains)) {
             extraTrains.push(trn);
             continue;
         }
         const trainTimetable = todaysTimetable[trn];
         const secsOffTimetable: number[] = [];
         if (data.status.timesAPI) {
-            activeTrainsFromTimesAPI.push(trn);
             const parsedLocation = parseTimesAPILocation(data.status.timesAPI.lastEvent.location);
             if (parsedLocation) {
                 secsOffTimetable.push(calculateDifferenceToTimetable(
@@ -46,7 +51,6 @@ export default async function command(interaction: CommandInteraction) {
             }
         }
         if (data.status.trainStatusesAPI) {
-            activeTrainsFromTrainStatusesAPI.push(trn);
             const parsedLastSeen = parseLastSeen(data.status.trainStatusesAPI.lastSeen);
             if (parsedLastSeen) {
                 secsOffTimetable.push(calculateDifferenceToTimetable(
@@ -62,22 +66,14 @@ export default async function command(interaction: CommandInteraction) {
                 .filter(isFinite)
                 .map(secs => Math.round(secs / 60))
         )];
-        if (minsOffTimetable.some(delay => delay >= 6)) {
-            lateTrains.push({
-                trn,
-                possibleDelays: minsOffTimetable
-            });
-        }
-        if (minsOffTimetable.some(delay => delay <= -6)) {
-            earlyTrains.push({
+        if (minsOffTimetable.some(delay => Math.abs(delay) >= 6)) {
+            delayedTrains.push({
                 trn,
                 possibleDelays: minsOffTimetable
             });
         }
     }
-
-    lateTrains.sort((a, b) => Math.max(...a.possibleDelays) - Math.max(...b.possibleDelays));
-    earlyTrains.sort((a, b) => Math.min(...b.possibleDelays) - Math.min(...a.possibleDelays));
+    delayedTrains.sort((a, b) => Math.max(...a.possibleDelays) - Math.max(...b.possibleDelays));
 
     const embed = new EmbedBuilder()
         .addFields(
@@ -94,12 +90,10 @@ export default async function command(interaction: CommandInteraction) {
                 value: listTrains(timetabledTrains)
             },
             {
-                name: `Potentially late trains (${lateTrains.length})`,
-                value: lateTrains.map(train => `${train.trn} (${train.possibleDelays.join('m or ')}m)`).join(', ')
-            },
-            {
-                name: `Potentially early trains (${earlyTrains.length})`,
-                value: earlyTrains.map(train => `${train.trn} (${train.possibleDelays.join('m or ')}m)`).join(', ')
+                name: `Potentially delayed trains (${delayedTrains.length})`,
+                value: delayedTrains.length
+                    ? delayedTrains.map(train => `${train.trn} (${train.possibleDelays.map(d => `${d<0?'':'+'}${d}m`).join(' or ')})`).join(', ')
+                    : 'None'
             },
             {
                 name: `Missing trains (${missingTrains.length})`,
